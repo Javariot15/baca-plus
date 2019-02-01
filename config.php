4275b9681a23e63e3abb251ed1808a0428877354
lupum165 <46644689+lupum165@users.noreply.github.com>
2019-02-01 20:09:42 +0700

    Update config.php

--- config.php
+++ config.php
@@ -5,9 +5,9 @@ $uid="Facebook_401164430647496";
 $raw_uid="d40cf7f4-6696-4952-ac9b-43a5d2a4c3a5";
 $user_name="Minhajuna+Ulumudin";
 $invite_code="F1puiw0L7Y";
-$own_mac="3c%3Ab6%3Ab7%3A22%3Add%3Aa7";
-$android_id="a7ad1d62665ba4b3";
-$device_type="vivo++vivo+Y21";
+$own_mac="02%3A00%3A00%3A00%3A00%3A00";
+$android_id="6912c3a6c7981e41";
+$device_type="Xiaomi++Redmi+4A";
 
 
 ?>
